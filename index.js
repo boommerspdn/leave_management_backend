f8140c420c387b2595fbc178ad760c87e996309c
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2023-09-11 22:42:46 +0700

    add crud for employee

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 const express = require('express')
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient, Prisma } = require('@prisma/client')
 
 
 const prisma = new PrismaClient()
 
 const app = express()
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/employee', async (req, res) => {
-    const allEmployee = await prisma.employee.findMany({
-        include: {
-            dep: true,
-            time_record: true,
-            approval_docs: true
-        }
-    });
-
-    res.json(allEmployee)
-    console.log(allEmployee)
-});
-
-app.get('/api/approval', async (req, res) => {
-    const allApproval = await prisma.approval_doc.findMany({
-        select: {
-            reason: true,
-            type: {
-              select: {
-                type_name: true,
-              },
-            },
-          },
-    });
-
-    res.json(allApproval)
-    console.log(allApproval)
-});
+// Home Page Route
+app.get('/', (req, res) =>
+  res.send('Leave Management API')
+);
 
 
+// Api Routes
+app.use('/api/AdminDashboard', require('./routes/api/AdminDashboard'));
+app.use('/api/ClockInOut', require('./routes/api/ClockInOut'));
+app.use('/api/DayOff', require('./routes/api/DayOff'));
+app.use('/api/department', require('./routes/api/department'));
+app.use('/api/DepartmentDashboard', require('./routes/api/DepartmentDashboard'));
+app.use('/api/employee', require('./routes/api/employee'));
+app.use('/api/LeaveDocument', require('./routes/api/LeaveDocument'));
+app.use('/api/LeaveStatistic', require('./routes/api/LeaveStatistic'));
+app.use('/api/LeaveType', require('./routes/api/LeaveType'));
 
 
-const server = app.listen(3000)
+// Global function for error handling
+global.checkingValidationError = (e, req, res) => {
+  if (e instanceof Prisma.PrismaClientValidationError) {
+      console.log(e)
+      res.status(422).json({ status: 422, message: 'Incorrect field type provided or missing an input' })
+  } else if (e.meta) {
+      console.log(e)
+      res.status(404).json({ status: 404, message: e.meta.cause })
+  }
+  else {
+      console.log(e)
+      res.status(400).json({ status: 400, message: "Error Occured" })
+  }
+}
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ routes/api/employee.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+// Create employee
+router.post('/', async (req, res) => {
+    const { firstName, lastName, department, birthDate, email, phone, gender, address, username, password, auth } = req.body;
+
+    try {
+        const createEmployee = await prisma.employee.create({
+            data: {
+                first_name: firstName,
+                last_name: lastName,
+                dep_id: parseInt(department),
+                birth_date: birthDate && new Date(birthDate),
+                email: email,
+                phone: phone,
+                gender: gender,
+                address: address,
+                username: username,
+                password: password,
+                auth: auth ? parseInt(auth) : 0
+            },
+        })
+
+        res.status(201).json(createEmployee)
+    } catch (e) {
+        checkingValidationError(e, req, res)
+    }
+});
+
+// Read all employees
+router.get('/', async (req, res) => {
+    const allEmployee = await prisma.employee.findMany({
+        include: {
+            dep: true
+        }
+    });
+
+    res.json(allEmployee);
+});
+
+// Read single employee
+router.get('/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    try {
+        const singleEmployee = await prisma.employee.findUnique({
+            where: {
+                id: id
+            },
+            include: {
+                dep: true
+            }
+        });
+        
+        res.status(200).json(singleEmployee)
+    } catch (e) {
+        checkingValidationError(e, req, res)
+    }
+
+});
+
+// Update employee info
+router.put('/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    const { firstName, lastName, department, birthDate, email, phone, gender, address, username, password, auth } = req.body;
+
+    try {
+        const editEmployee = await prisma.employee.update({
+            where: {
+                id: id
+            },
+            data: {
+                first_name: firstName,
+                last_name: lastName,
+                dep_id: department ? parseInt(department) : undefined,
+                birth_date: birthDate ? new Date(birthDate) : undefined,
+                email: email,
+                phone: phone,
+                gender: gender,
+                address: address,
+                username: username,
+                password: password,
+                auth: auth ? parseInt(auth) : undefined
+            }
+        })
+
+        res.status(200).json({status: 200, message: `Record id ${id} successfully updated`})
+    } catch (e) {
+        checkingValidationError(e, req, res)
+    }
+})
+
+// Delete employee info
+router.delete('/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+
+    try {
+        const deleteSingleEmployee = await prisma.employee.delete({
+            where: {
+                id: id
+            },
+        });
+
+        res.status(200).json({status: 200, message: `Record id ${id} successfully deleted`})
+    } catch (e) {
+        checkingValidationError(e, req, res)
+    }
+})
+
+// Edit employee info
+module.exports = router;
