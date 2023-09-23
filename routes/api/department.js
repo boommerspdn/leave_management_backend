fbc00ab487715737de5e9205d9e9ba44b0165b07
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2023-09-23 21:50:49 +0700

    remove head

--- routes/api/department.js
+++ routes/api/department.js
@@ -1,90 +1,104 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 // Create department
-router.post('/', async (req, res) => {
-    const { departmentName, headDep } = req.body;
+router.post("/", async (req, res) => {
+  const { departmentName } = req.body;
 
-    try {
-        const createDepartment = await prisma.department.create({
-            data: {
-                dep_name: departmentName,
-                head_dep: parseInt(headDep)
-            },
-        })
+  try {
+    const createDepartment = await prisma.department.create({
+      data: {
+        dep_name: departmentName,
+      },
+    });
 
-        res.status(201).json({ status: 201, message: 'Record has been created', createDepartment })
-    } catch (e) {
-        checkingValidationError(e, req, res)
-    }
+    res
+      .status(201)
+      .json({
+        status: 201,
+        message: "Record has been created",
+        createDepartment,
+      });
+  } catch (e) {
+    checkingValidationError(e, req, res);
+  }
 });
 
 // Read all departments
-router.get('/', async (req, res) => {
-    const allDepartment = await prisma.department.findMany({
-    });
+router.get("/", async (req, res) => {
+  const allDepartment = await prisma.department.findMany({});
 
-    res.status(200).json(allDepartment);
+  res.status(200).json(allDepartment);
 });
 
 // Read single department
-router.get('/:id', async (req, res) => {
-    const id = parseInt(req.params.id)
-    try {
-        const singleDepartment = await prisma.department.findUnique({
-            where: {
-                id: id
-            },
-        });
+router.get("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const singleDepartment = await prisma.department.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
-        if (singleDepartment == null) return res.status(404).json({ status: 404, msg: `no record with id of ${id}` })
-        res.status(200).json(singleDepartment)
-    } catch (e) {
-        checkingValidationError(e, req, res)
-    }
+    if (singleDepartment == null)
+      return res
+        .status(404)
+        .json({ status: 404, msg: `no record with id of ${id}` });
+    res.status(200).json(singleDepartment);
+  } catch (e) {
+    checkingValidationError(e, req, res);
+  }
 });
 
 // Update department info
-router.put('/:id', async (req, res) => {
-    const id = parseInt(req.params.id)
-    const { departmentName, headDep } = req.body;
+router.put("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { departmentName, headDep } = req.body;
 
-    try {
-        const editDepartment = await prisma.department.update({
-            where: {
-                id: id
-            },
-            data: {
-                dep_name: departmentName,
-                head_dep: parseInt(headDep)
-            }
-        })
+  try {
+    const editDepartment = await prisma.department.update({
+      where: {
+        id: id,
+      },
+      data: {
+        dep_name: departmentName,
+        head_dep: parseInt(headDep),
+      },
+    });
 
-        res.status(200).json({ status: 200, message: `Record id ${id} successfully updated`, editDepartment })
-    } catch (e) {
-        checkingValidationError(e, req, res)
-    }
-})
+    res
+      .status(200)
+      .json({
+        status: 200,
+        message: `Record id ${id} successfully updated`,
+        editDepartment,
+      });
+  } catch (e) {
+    checkingValidationError(e, req, res);
+  }
+});
 
 // Delete department info
-router.delete('/:id', async (req, res) => {
-    const id = parseInt(req.params.id)
+router.delete("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    try {
-        const deleteSingleDepartment = await prisma.department.delete({
-            where: {
-                id: id
-            },
-        });
+  try {
+    const deleteSingleDepartment = await prisma.department.delete({
+      where: {
+        id: id,
+      },
+    });
 
-        res.status(200).json({ status: 200, message: `Record id ${id} successfully deleted` })
-    } catch (e) {
-        checkingValidationError(e, req, res)
-    }
-})
+    res
+      .status(200)
+      .json({ status: 200, message: `Record id ${id} successfully deleted` });
+  } catch (e) {
+    checkingValidationError(e, req, res);
+  }
+});
 
 module.exports = router;
