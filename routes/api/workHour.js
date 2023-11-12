fea3bb4b4148bf89256e95a4e250d74fe5520cf5
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2023-11-12 20:05:17 +0700

    move time calc to front

--- routes/api/workHour.js
+++ routes/api/workHour.js
@@ -7,31 +7,19 @@ const prisma = require("../../client");
 router.post("/", async (req, res) => {
   const { start_time, end_time } = req.body;
 
-  const startTime = new Date();
-  const endTime = new Date();
-
-  const startHour = parseInt(start_time.slice(0, 2));
-  const startMinute = parseInt(start_time.slice(3, 4));
-
-  const endHour = parseInt(end_time.slice(0, 2));
-  const endMinute = parseInt(end_time.slice(3, 4));
-
-  startTime.setUTCHours(startHour, startMinute, 0, 0);
-  endTime.setUTCHours(endHour, endMinute, 0, 0);
-
   try {
     const upsertWorkHour = await prisma.work_hour.upsert({
       where: {
         id: 1,
       },
       update: {
-        start_time: startTime,
-        end_time: endTime,
+        start_time: start_time,
+        end_time: end_time,
       },
       create: {
         id: 1,
-        start_time: startTime,
-        end_time: endTime,
+        start_time: start_time,
+        end_time: end_time,
       },
     });
 
